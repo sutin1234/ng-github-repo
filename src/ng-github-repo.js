95dca4952364a7487b09f7071f699489e203667a
sutin1234 <tony.sutin1234@gmail.com>
2019-10-03 13:05:17 +0700

    fixed require

--- src/ng-github-repo.js
+++ src/ng-github-repo.js
@@ -1,4 +1,4 @@
-const axios = reqire('axios').default;
+const axios = require('axios').default;
 
 module.exports = (language) => {
     return axios.get(`https://api.github.com/search/repositories?q=stars:%3E=10000+language:${language}&sort=stars&order=desc`);
